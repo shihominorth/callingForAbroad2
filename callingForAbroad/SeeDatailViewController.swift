8f8aadd108e85c2958fe37ae8c7da1a4002cd27a
Shihomi Kitajima <shihomi0104@icloud.com>
2020-02-04 14:30:28 -0800

    enable to show names and addresses of cafes. tap the marker and then lanuch google maps app

---
+++ callingForAbroad/SeeDatailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SeeDatailViewController.swift
+//  checklist
+//
+//  Created by 北島　志帆美 on 2020-02-04.
+//  Copyright © 2020 北島　志帆美. All rights reserved.
+//
+
+import UIKit
+
+class SeeDatailViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var seeDetailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

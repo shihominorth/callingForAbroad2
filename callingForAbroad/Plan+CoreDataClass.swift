e4dbbdf7a3bf740179937c3b8c1cb3c85af87b54
Shihomi Kitajima <shihomi0104@icloud.com>
2020-04-10 21:09:36 -0700

    commit some files that couldn't stage

---
+++ callingForAbroad/Plan+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Plan+CoreDataClass.swift
+//  checklist
+//
+//  Created by 北島　志帆美 on 2020-02-14.
+//  Copyright © 2020 北島　志帆美. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Plan: NSManagedObject {
+
+}

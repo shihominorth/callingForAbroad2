45473526b861b356458f81b578a9e53d7e312f05
Shihomi Kitajima <shihomi0104@icloud.com>
2020-04-10 19:59:26 -0700

    move files

---
+++ callingForAbroad/functionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  functionTableViewCell.swift
+//  checklist
+//
+//  Created by 北島　志帆美 on 2019-12-19.
+//  Copyright © 2019 北島　志帆美. All rights reserved.
+//
+
+import UIKit
+
+class functionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
